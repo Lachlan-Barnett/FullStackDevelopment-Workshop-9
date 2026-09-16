a88540f239b0103e4680ca52b0e767d693f6b337
Anoraxium1 <267790205+Anoraxium1@users.noreply.github.com>
2026-09-17 09:39:44 +1000

    Changed the code style

--- App/add.js
+++ App/add.js
@@ -1,17 +1,21 @@
-const products = [
+const docArray = [
     { id: 1, name: '16GB USB', description: '16GB USB flash drive', price: 8.99, units: 30 },
     { id: 2, name: '32GB USB', description: '32GB USB flash drive', price: 14.99, units: 20 },
     { id: 3, name: '64GB USB', description: '64GB USB flash drive', price: 24.99, units: 15 }
 ];
 
-async function addProducts(db) {
-    const collection = db.collection('products');
+exports.insertDocuments = function(collection, docArray, callback) {
+    collection.insertMany(docArray, function(err, result) {
+        console.log("Inserted the following documents into the collection:");
+        console.log(docArray);
+        callback(result);
+    });
+};
 
-    await collection.drop().catch(() => {});
-
-    const result = await collection.insertMany(products);
-    console.log(`Inserted ${result.intertedCount} products: `);
-    console.log(products);
-}
-
-module.exports = addProducts;
+module.exports = function(collection) {
+    collection.drop(function() {
+        exports.insertDocuments(collection, docArray, function(result) {
+            console.log('add.js done.');
+        });
+    });
+};
--- App/app.js
+++ App/app.js
@@ -1,5 +1,4 @@
 const { MongoClient } = require('mongodb');
-const addProducts = require('./add');
 
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
@@ -9,10 +8,10 @@ const dbName = 'mydb';
 async function main() {
     await client.connect();
     console.log('Connected successfully to server');
-
     const db = client.db(dbName);
+    const collection = db.collection('products');
     
-    await addProducts(db);
+    require('./add')(collection);
 
     return 'done.';
 }
