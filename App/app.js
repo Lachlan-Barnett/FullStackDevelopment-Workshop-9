0a9e8ee3efd7a07369b05caa1cbdfb508badd761
Anoraxium1 <267790205+Anoraxium1@users.noreply.github.com>
2026-09-17 09:20:55 +1000

    Added code to app.js

---
+++ App/app.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+const dbName = 'mydb';
+async function main() {
+    await client.connect();
+    console.log('Connected successfully to server');
+
+    const db = client.db(dbName);
+    const collection = db.collection('products');
+
+    return 'done.';
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
