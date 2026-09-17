f71a3a3fa4db484f34224188fd0d6bded5eb12af
Anoraxium1 <267790205+Anoraxium1@users.noreply.github.com>
2026-09-17 10:31:15 +1000

    Added code to the angular

--- products-app/src/app/app.routes.ts
+++ products-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Products } from './products/products';
+import { AddProduct } from './add-product/add-product';
+import { UpdateProduct } from './update-product/update-product';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Products },
+    { path: 'add', component: AddProduct },
+    { path: 'update', component: UpdateProduct }
+];
