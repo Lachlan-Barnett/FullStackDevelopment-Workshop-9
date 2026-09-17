ed5270a635c264f91f50d99336bb9eb179d3710f
Anoraxium1 <267790205+Anoraxium1@users.noreply.github.com>
2026-09-17 10:23:25 +1000

    Code fixes to server.js

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { MongoServerSelectionError } = require('mongodb');
-const { MongoClient } = requre('mongodb');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,7 +9,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url, { MongoServerSelectionTimeoutMS: 5000});
+const client = new MongoClient(url, { serverSelectionTimeoutMS: 5000 });
 const dbName = 'mydb';
 const PORT = 3000;
 
@@ -19,14 +18,15 @@ async function main() {
     console.log('Connected successfully to server');
     const db = client.db(dbName);
 
-    require('./routes/read')(db, app);
-    require('./routes/add')(db, app);
-    require('./routes/update')(db, app);
-    require('./routes/remove')(db, app);
+    require('./read')(db, app);
+    require('./add')(db, app);
+    require('./update')(db, app);
+    require('./remove')(db, app);
 
     app.listen(PORT, () => {
         console.log(`Starting server at: ${PORT}`);
     });
 }
 
-main().catch(console.error);
+main()
+    .catch(console.error);
